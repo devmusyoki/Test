ef071e6ac85a8cf7f000f1d042c15a98261f8019
smusyoki <markmusyoki6@gmail.com>
2022-03-17 14:02:43 +0300

    update

--- testing/admin.py
+++ testing/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from testing.models import Category, Type, Name
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Type)
+admin.site.register(Name)
--- testing/models.py
+++ testing/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Name(models.Model):
+    Title = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name_plural = "Name"
+
+    def __str__(self):
+        return f"{self.Title}"
+
+
+class Type(models.Model):
+    rows = models.BooleanField(default=False)
+    dropdown = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Type"
+
+    def __str__(self):
+        return f"{self.rows}{self.dropdown}"
+
+
+class Category(models.Model):
+    Entries = models.ManyToManyField(Name, blank=False)
+    Title = models.CharField(max_length=20)
+    Type = models.OneToOneField(Type, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return f"{self.Title}"
